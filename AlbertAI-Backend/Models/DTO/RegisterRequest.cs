971b216736029ef1533f88d5cad995856830ec3a
Shordore <s.darioosh@ufl.edu>
2025-02-08 21:30:36 -0500

    Add initial backend project structure and configuration files. Login and registration working.

---
+++ AlbertAI-Backend/Models/DTO/RegisterRequest.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AlbertAI.Models.DTO
+{
+
+    public class RegisterRequest
+
+    {
+
+        public string UFID { get; set; }
+
+        public string Password { get; set; }
+
+        public string Name { get; set; }
+
+
+    }
+}
