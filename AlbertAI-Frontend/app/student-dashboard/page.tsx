b543c6ded45ed32a574c1e7dd2a09523ca2d66ad
JakeKantor <jacobmk8@gmail.com>
2025-02-13 16:09:04 -0500

    made student dashboard placeholder

---
+++ AlbertAI-Frontend/app/student-dashboard/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function StudentDashboardPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black text-white">
+      <h1 className="text-4xl font-bold">Student Dashboard</h1>
+    </div>
+  );
+}
