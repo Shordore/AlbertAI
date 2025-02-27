b04176c69af80e360bee50052ec21ab53e686640
SeanHersh <32366781+SeanHersh@users.noreply.github.com>
2025-02-26 23:17:55 -0500

    Added Flashcard feature

--- AlbertAI-Backend/obj/Debug/net8.0/MyBackend.AssemblyInfo.cs
+++ AlbertAI-Backend/obj/Debug/net8.0/MyBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+020ec44a1bf71f6f397d3d67d620fa50310105f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+073f9e92725d3565591df483b70f83e2c57b7dfa")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
