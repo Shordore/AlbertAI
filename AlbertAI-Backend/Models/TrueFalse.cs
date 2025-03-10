2e0db040719ff84123422d0b466026e3c9ada5b5
Shordore <s.darioosh@ufl.edu>
2025-03-09 22:43:41 -0400

    Fixed missing Flashcard file, and modified True/False to work as intended.

--- AlbertAI-Backend/Models/TrueFalse.cs
+++ AlbertAI-Backend/Models/TrueFalse.cs
@@ -4,31 +4,18 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AlbertAI.Models
 {
-    public class TrueFalse
-    {
-        [Key]
-        public int Id { get; set; } // Unique identifier
+        public class TrueFalse
+        {
 
-        [Required]
-        public string Question { get; set; } // Flashcard question
+                // New properties to add
+                public DateTime CreatedAt { get; set; }
+                public DateTime? LastReviewed { get; set; }
+                public int TimesReviewed { get; set; }
+                public double SuccessRate { get; set; }
+                [Required]
+                public int ClassCodeId { get; set; } // Foreign key to ClassCode
 
-        [Required]
-        public bool Answer { get; set; }  // true or false
-
-        public string Category { get; set; } // Optional category
-
-<<<<<<< HEAD:AlbertAI-Backend/Models/TrueFalse.cs
-        // New properties to add
-        public DateTime CreatedAt { get; set; }
-        public DateTime? LastReviewed { get; set; }
-        public int TimesReviewed { get; set; }
-        public double SuccessRate { get; set; }
-=======
-        [Required]
-        public int ClassCodeId { get; set; } // Foreign key to ClassCode
-
-        [ForeignKey("ClassCodeId")]
-        public ClassCode ClassCode { get; set; }
->>>>>>> a2b5bf284b9bf67497a4ce8ad3ff680a9f8b7ac4:AlbertAI-Backend/Models/Flashcard.cs
-    }
+                [ForeignKey("ClassCodeId")]
+                public ClassCode ClassCode { get; set; }
+        }
 }
